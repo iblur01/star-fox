55f18898dc272c00aafbb0b8786a9ba48358a145
iblur01 <theo.delannoy@reseau.eseo.fr>
2022-10-19 11:14:50 +0200

    creation plateau

--- src/main/java/fr/eseo/bachelor/starfox/HelloApplication.java
+++ src/main/java/fr/eseo/bachelor/starfox/HelloApplication.java
@@ -1,16 +1,33 @@
 package fr.eseo.bachelor.starfox;
 
-import fr.eseo.bachelor.starfox.cases.Cases;
+
+import fr.eseo.bachelor.starfox.affichage.Plateau;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static java.lang.Long.MAX_VALUE;
+
 public class HelloApplication extends Application {
+
+
+    private final int WIDTH = 700;
+    private final int HEIGHT = 700;
+
     @Override
     public void start(Stage stage) throws IOException {
+
+
+        Plateau plateau = new Plateau();
+
+        Scene scene = new Scene(plateau.getPlateau(), WIDTH, HEIGHT);
+        stage.setScene(scene);
+        //stage.setResizable(false);
+        stage.show();
 
     }
 
