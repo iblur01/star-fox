6a23154ec8de743a161b388d292fa35b84ef39b2
NightCorvus <bab1nono85@gmail.com>
2022-10-26 17:05:01 +0200

    menu

--- src/main/java/fr/eseo/bachelor/starfox/HelloApplication.java
+++ src/main/java/fr/eseo/bachelor/starfox/HelloApplication.java
@@ -1,6 +1,7 @@
 package fr.eseo.bachelor.starfox;
 
 
+import fr.eseo.bachelor.starfox.affichage.Menu;
 import fr.eseo.bachelor.starfox.affichage.Plateau;
 import javafx.application.Application;
 import javafx.scene.Node;
@@ -18,15 +19,30 @@ public class HelloApplication extends Application {
     private final int WIDTH = 700;
     private final int HEIGHT = 700;
 
+    private final int WIDTHm = 500;
+    private final int HEIGHTm = 500;
+
     @Override
     public void start(Stage stage) throws IOException {
 
 
-        Plateau plateau = new Plateau();
-        plateau.init();
 
-        Scene scene = new Scene(plateau.getPlateau(), WIDTH, HEIGHT);
-        stage.setScene(scene);
+        Menu menu = new Menu();
+        menu.initMenu();
+        menu.initMenu();
+
+
+
+
+        Plateau plateau = new Plateau();
+        Scene scene1 = new Scene(menu.initMenu(), WIDTHm, HEIGHTm);
+        Scene scene2 = new Scene(plateau.getPlateau(), WIDTH, HEIGHT);
+
+        start.setOnAction(e -> stage.setScene(scene2));
+
+
+
+        stage.setScene(scene1);
         //stage.setResizable(false);
         stage.show();
 
---
+++ src/main/java/fr/eseo/bachelor/starfox/affichage/Menu.java
@@ -0,0 +1,107 @@
+package fr.eseo.bachelor.starfox.affichage;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Menu {
+    private final int WIDTH = 500;
+    private final int HEIGHT = 500;
+    private GridPane root;
+    private Label label;
+
+    private File buttonPara;
+    private Image imageButtonPara;
+    private ImageView imageViewButtonPara;
+
+    private File file;
+    private Image image;
+    private ImageView imageView;
+
+    private File fileStart;
+    private Image imageStart;
+    private ImageView imageViewStart;
+
+
+
+
+
+    public GridPane initMenu(){
+
+        root = new GridPane();
+        root.setStyle("-fx-background-color:lightblue");
+        root.setAlignment(Pos.TOP_CENTER);
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(20);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(60);
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(20);
+        root.getColumnConstraints().addAll(column1, column2, column3);
+        RowConstraints row1 = new RowConstraints(10);
+        row1.setPercentHeight(33);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(34);
+        RowConstraints row3 = new RowConstraints();
+        row3.setPercentHeight(33);
+        root.getRowConstraints().addAll(row1, row2, row3);
+
+        buttonPara = new File("img/button_para.png");
+        imageButtonPara = new Image(buttonPara.toURI().toString(),0.05*WIDTH,0.05*HEIGHT,false,false);
+        imageViewButtonPara = new ImageView(imageButtonPara);
+
+        file = new File("img/monopLabel.png");
+        image = new Image(file.toURI().toString(),0.6*WIDTH,0.2*HEIGHT,false,false);
+        imageView = new ImageView(image);
+
+        fileStart = new File("img/start.png");
+        imageStart = new Image(fileStart.toURI().toString(),0.4*WIDTH,0.15*HEIGHT,false,false);
+        imageViewStart = new ImageView(imageStart);
+
+        ComboBox<String> nombreJoueur = new ComboBox<>();
+        nombreJoueur.getItems().addAll("2 joueurs","3 joueurs","4 joueurs");
+
+        Button start = new Button();
+        start.setGraphic(imageViewStart);
+
+
+
+        Button para = new Button();
+        para.setGraphic(imageViewButtonPara);
+
+        Label titre = new Label();
+        titre.setGraphic(imageView);
+
+        VBox choixJoueur = new VBox();
+        choixJoueur.getChildren().add(nombreJoueur);
+        choixJoueur.setAlignment(Pos.TOP_CENTER);
+        root.add(choixJoueur,1,2);
+
+        HBox game = new HBox();
+        game.getChildren().add(start);
+        game.setAlignment(Pos.CENTER);
+        root.add(game,1,1);
+
+        HBox top = new HBox();
+        top.getChildren().add(titre);
+        top.setAlignment(Pos.CENTER);
+        root.add(top,1,0);
+
+        VBox parametre = new VBox();
+        parametre.getChildren().add(para);
+        parametre.setAlignment(Pos.TOP_RIGHT);
+        root.add(parametre,2,0);
+
+        return root;
+
+    }
+}
