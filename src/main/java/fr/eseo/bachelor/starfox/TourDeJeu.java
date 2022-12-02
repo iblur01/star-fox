dbb64ddecdfc22f49460467d92845b2a15bce113
NightCorvus <bab1nono85@gmail.com>
2022-12-02 14:30:12 +0100

    Logic plateau

--- src/main/java/fr/eseo/bachelor/starfox/TourDeJeu.java
+++ src/main/java/fr/eseo/bachelor/starfox/TourDeJeu.java
@@ -29,7 +29,7 @@ public class TourDeJeu {
 
     public void init_TDJ(int nb_j, String name1, String name2,String name3,String name4 ){
 
-        init_cases();
+
         if (nb_j>1){
 
             Joueur1.setEnable(true);
@@ -79,46 +79,7 @@ public class TourDeJeu {
         joueur_actuel = 1;
     }
 
-    public void init_cases(){
-        init_compagnies();
-        init_rues();
-        init_gares();
-    }
 
-    public void init_rues(){
-
-        Rues rue1 = new Rues(1,"O'Tacos",60,30);
-        Rues rue2 = new Rues(3,"McDo",60,30);
-        Rues rue3 = new Rues(6,"Point Chicken",100,350);
-        Rues rue4 = new Rues(8,"Food 49",100,50);
-        Rues rue5 = new Rues(9,"Mc Donner",120,60);
-        Rues rue6 = new Rues(11,"La Chapelle",140,70);
-        Rues rue7 = new Rues(13,"Le Carré",140,70);
-        Rues rue8 = new Rues(14,"Le Mid Star",160,80);
-        Rues rue9 = new Rues(16,"Le Prime",180,90);
-        Rues rue10 = new Rues(18,"So Sushi",180,90);
-        Rues rue11 = new Rues(19,"Fresh Burritos",200,100);
-        Rues rue12 = new Rues(21,"L'Atoll",220,110);
-        Rues rue13 = new Rues(23,"L'esapce Anjou",220,110);
-        Rues rue14 = new Rues(24,"Grand Maine",240,120);
-        Rues rue15 = new Rues(26,"Lac de Maine",260,130);
-        Rues rue16 = new Rues(27,"Aqua Vita",260,130);
-        Rues rue17 = new Rues(29,"Les sablières",280,140);
-        Rues rue18 = new Rues(31,"Le jardin des Plantes",300,150);
-        Rues rue19 = new Rues(32,"Le jardin du Mail",300,150);
-        Rues rue20 = new Rues(34,"Terra Botanica",320,160);
-        Rues rue21 = new Rues(37,"Le Chateau d'Angers",350,175);
-        Rues rue22 = new Rues(39,"ESEO",400,200);
-
-    }
-    public void init_gares(){
-
-
-    }
-    public void init_compagnies(){
-
-
-    }
 
     private void popup_joueur(Joueur joueurX){
 
